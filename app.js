5c16dee89d7194b3f201f3da6895af35ad93330f
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-06-29 23:24:49 -0500

    Operator Precedence

---
+++ app.js
@@ -0,0 +1,10 @@
+// Operator precedence describes the order in which operations are performed in an arithmetic expression.
+document.getElementById("demo").innerHTML = 100 + 50 * 3;
+
+/*
+When using parentheses, the operations inside the parentheses are computed first.
+*/
+document.getElementById("demo1").innerHTML = (100 + 50) * 3;
+
+// When using parentheses, the operations inside the parentheses are computed first.
+document.getElementById("demo2").innerHTML = 100 + 50 -3;
